9f03da2210911cf6041adde3163307ce85505f3c
AVANZA\raza.ali1 <raza.ali1@avanzasolutions.com>
2026-04-27 23:17:25 +0500

    Route and config related fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import { RouterProvider } from "@tanstack/react-router";
+import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />,
 );
